3cb54811349193ddcbc1a14e3309bb4d6626278e
AndreySamaev <andreysamaev@gmail.com>
2025-04-26 16:18:39 +0300

    add test counter

--- components/Alerts.vue
+++ components/Alerts.vue
@@ -2,11 +2,9 @@
 </script>
 
 <template>
- <div class="card">
-   <h1 class="bg-primary">This is alert Component</h1>
- </div>
+  <div class="card">
+    <h1 class="bg-primary">This is alert Component</h1>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,23 @@
+vue
 <script setup>
- 
+ useHead({
+  title: 'Test'
+ })
+ let counter = ref(1);
+ const increaseCounter = () => {
+  counter.value++
+ }
+onMounted(() => {
+  setInterval(() => {
+    increaseCounter();
+  }, 1000)
+ })
 </script>
-
 <template>
   <h2 class="text-center">Index</h2>
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam doloremque dolores ea, eum id illum inventore libero magnam natus nisi non possimus quaerat quam recusandae repellendus sequi tempora unde?
 <h3>Test</h3>
-<h3>Test</h3>
-<h3>Test</h3>
-<h3>Test</h3>
-<h3>Test</h3>
-<h3>Test</h3>
+<p>{{ counter }}</p>
 </template>
-
 <style scoped>
-
 </style>
